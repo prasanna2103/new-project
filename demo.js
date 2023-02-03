58fe8edbc3cc3078e86073cefc0a6963d0f821ac
prasanna2103 <ayitharaju.prasanna2103@gmail.com>
2023-02-03 16:07:47 +0530

    local stoarge added

--- demo.js
+++ demo.js
@@ -1,100 +1,73 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+// Get form element
+const form = document.getElementById('addForm');
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
-
-// Add item
-function addItem(e){
+// Add submit event listener to form
+form.addEventListener('submit', function(e) {
+  // Prevent default form submit behavior
   e.preventDefault();
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
+  // Get input elements
+  const item = document.getElementById('item');
+  const description = document.getElementById('description');
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  // Get items from local storage
+  let items = JSON.parse(localStorage.getItem('items')) || [];
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
+  // Create object to store item and description
+  const itemData = {
+    name: item.value,
+    description: description.value
+  };
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  // Push item data to items array
+  items.push(itemData);
 
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+  // Store items array in local storage
+  localStorage.setItem('items', JSON.stringify(items));
 
-  // Append button to li
-  li.appendChild(deleteBtn);
+  // Clear input values
+  item.value = '';
+  description.value = '';
 
-  // Append li to list
-  itemList.appendChild(li);
-}
+  // Call function to display items
+  displayItems();
+});
 
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
+// Function to display items
+function displayItems() {
+  // Get items from local storage
+  let items = JSON.parse(localStorage.getItem('items')) || [];
 
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
+  // Get items list element
+  const itemsList = document.getElementById('items');
+
+  // Clear items list
+  itemsList.innerHTML = '';
+
+  // Loop through items array
+  items.forEach(function(item) {
+    // Create list item element
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+
+    // Create delete button element
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.innerHTML = 'X';
+
+    // Append item name and delete button to list item
+    li.innerHTML = item.name;
+    li.appendChild(deleteBtn);
+
+    // Append item description to list item
+    const desc = document.createElement('p');
+    desc.innerHTML = item.description;
+    li.appendChild(desc);
+
+    // Append list item to items list
+    itemsList.appendChild(li);
   });
 }
 
-const filter = document.querySelector("#filter");
-      const itemsList = document.querySelector("#items");
-      const addForm = document.querySelector("#addForm");
-
-      filter.addEventListener("input", function() {
-        const searchString = filter.value.toLowerCase();
-        const items = itemsList.querySelectorAll("li");
-
-        for (let i = 0; i < items.length; i++) {
-          const item = items[i];
-          const itemName = item.querySelector(".item-name").textContent.toLowerCase();
-          const itemDesc = item.querySelector(".item-desc").textContent.toLowerCase();
-          if (itemName.includes(searchString) || itemDesc.includes(searchString)) {
-            item.style.display = "block";
-          } else {
-            item.style.display = "none";
-          }
-        }
-      });
-
-      addForm.addEventListener("submit", function(e) {
-        e.preventDefault();
-        const itemName = addForm.querySelector("#item-name").value;
-        const itemDesc = addForm.querySelector("#item-desc").value;
-        const li = document.createElement("li");
-        li.innerHTML = `
-          <span class="item-name">${itemName}</span>
-          <p class="item-desc">${itemDesc}</p>
-          <button class="btn btn-danger btn-sm float-right delete">X</button>
-        `;
-        itemsList.appendChild(li);
-      });
+// Call function to display items on page load
+displayItems();
