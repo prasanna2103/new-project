91e57475d741aabc4e7abd2cc59b5030894639c2
prasanna2103 <ayitharaju.prasanna2103@gmail.com>
2023-02-03 11:30:34 +0530

    my first project

---
+++ demo.js
@@ -0,0 +1,6 @@
+var headerTitle = document.getElementById("header-title");
+headerTitle.style.border = "solid 1px black";
+
+var addItemTitle = document.querySelector(".title");
+addItemTitle.style.fontWeight = "bold";
+addItemTitle.style.color = "green";
