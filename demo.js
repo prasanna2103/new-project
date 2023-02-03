8e63173da16b85cbfd8a14a454cc3f415708782c
prasanna2103 <ayitharaju.prasanna2103@gmail.com>
2023-02-03 12:47:36 +0530

    added green color to list

--- demo.js
+++ demo.js
@@ -1,3 +1,10 @@
+var listItems = document.querySelectorAll("#items .list-group-item");
+listItems[2].style.backgroundColor = "green";
+
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontWeight = "bold";
+}
+
 var headerTitle = document.getElementById("header-title");
 headerTitle.style.border = "solid 1px black";
 
